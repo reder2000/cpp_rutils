435f8ba1c34252ddb0657f4c88a7f2be4e793a53
Richard Rouge <richard.rouge@outlook.com>
2021-03-02 00:38:42 +0100

    shared_ptr

---
+++ include/shared_ptr.h
@@ -0,0 +1,18 @@
+#pragma once 
+
+#include <memory>
+
+template <class _Ty>
+using sp = std::shared_ptr<_Ty>;
+
+template <class _Ty, class _OTy>
+inline
+sp<_Ty> make_sp_copy(const _OTy& t) {
+    return sp<_Ty>(new _Ty((_OTy(t))));
+}
+
+template <class _Ty, class _OTy>
+inline
+sp<_Ty> make_sp_move(_OTy&& t) {
+    return sp<_Ty>(new _Ty(std::move(t)));
+}
---
+++ tests/shared_ptr.cpp
@@ -0,0 +1,25 @@
+#include <catch.hpp>
+#include <shared_ptr.h>
+
+
+TEST_CASE("shared_ptr", "[enum][hide]")
+{
+    int i{ 2 };
+    std::shared_ptr<int> sp1(new int{ 2 });
+    auto sp2 = sp<int>(new int{ 2 });
+    CHECK(*sp1 == *sp2);
+    auto sp3 = make_sp_copy<int>(2.);
+    CHECK(*sp1 == *sp3);
+}
+
+TEST_CASE("shared_ptr_v", "[enum][hide]")
+{
+    using V = std::vector<int>;
+    V v1{ 1,2 };
+    std::shared_ptr<V> sp1(new V{ 1,2 });
+    auto sp2 = sp<V>(new V{ 1,2 });
+    CHECK(*sp1 == *sp2);
+    auto sp3 = make_sp_move<V>(v1);
+    CHECK(*sp1 == *sp3);
+    CHECK(v1.size() == 0);
+}
