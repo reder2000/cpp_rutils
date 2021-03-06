4e1ab31bb96c507cd51da0789d7ce47e03bf1dac
Richard Rouge <richard.rouge@outlook.com>
2021-03-07 00:18:37 +0100

    unsigned char literal

--- include/literals.h
+++ include/literals.h
@@ -3,8 +3,14 @@
 #include <cstddef>
 
 // size_t literal 
+inline
 constexpr size_t operator "" _uz(unsigned long long n)
 {
     return n;
 }
 
+
+inline constexpr unsigned char operator "" _uchar(char arg) noexcept
+{
+    return static_cast<unsigned char>(arg);
+}
