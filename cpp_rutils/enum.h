ca7e54d84c2a6c0bf4d1aa16dfd476acb249c876
Richard Rouge <rouge.richard@free.fr>
2022-04-08 09:54:07 +0200

    remove roost and add boost

--- cpp_rutils/enum.h
+++ cpp_rutils/enum.h
@@ -1,14 +1,14 @@
 #pragma once 
 
-//#include <roost/preprocessor/tuple.hpp>
-#include "roost/preprocessor/tuple/rem.hpp"
-#include "roost/preprocessor/cat.hpp"
-#include "roost/preprocessor/punctuation/comma_if.hpp"
-#include "roost/preprocessor/stringize.hpp"
-#include "roost/preprocessor/list/for_each_i.hpp"
-#include "roost/preprocessor/tuple/to_list.hpp"
-#include "roost/preprocessor/list/for_each.hpp"
-#include "roost/preprocessor/seq/elem.hpp"
+//#include <boost/preprocessor/tuple.hpp>
+#include "boost/preprocessor/tuple/rem.hpp"
+#include "boost/preprocessor/cat.hpp"
+#include "boost/preprocessor/punctuation/comma_if.hpp"
+#include "boost/preprocessor/stringize.hpp"
+#include "boost/preprocessor/list/for_each_i.hpp"
+#include "boost/preprocessor/tuple/to_list.hpp"
+#include "boost/preprocessor/list/for_each.hpp"
+#include "boost/preprocessor/seq/elem.hpp"
 #include <string_view>
 #include <fmt/format.h>
 #include "to_string.h"
@@ -21,19 +21,19 @@ T string_to_enum(std::string_view s);
 
 // name => name_tuple
 #define NAME_TUPLE(name) \
-	ROOST_PP_CAT(name,_tuple)
+	BOOST_PP_CAT(name,_tuple)
 
 
 // name_tuple = (a,b) =>
 // enum class name_tuple {a,b}
 #define EMIT_ENUM_CLASS_ENUM(name) \
 enum class name { \
-	ROOST_PP_TUPLE_REM_CTOR(NAME_TUPLE(name)) \
+	BOOST_PP_TUPLE_REM_CTOR(NAME_TUPLE(name)) \
 }
 
 // , data::elem , "elem"
 #define EMIT_ENUM_TO_STRING(r, data, i, elem) \
-	ROOST_PP_COMMA_IF(i) ROOST_PP_STRINGIZE(elem)
+	BOOST_PP_COMMA_IF(i) BOOST_PP_STRINGIZE(elem)
 
 // name_tuple = (a,b) =>
 // name2String map = { {a,"a"} , {b,"b"} }
@@ -41,17 +41,17 @@ enum class name { \
 template <> \
 inline const char*  enum_to_string<name>(name value)  { \
 	static const char* tab[] = { \
-	ROOST_PP_LIST_FOR_EACH_I(EMIT_ENUM_TO_STRING,name,ROOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))  \
+	BOOST_PP_LIST_FOR_EACH_I(EMIT_ENUM_TO_STRING,name,BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))  \
 	} ;\
 	return tab[static_cast<size_t>(value)] ;\
 }
 
-#define EMIT_STRING_TO_ENUM(r, data, elem) if (ROOST_PP_SEQ_ELEM(0,data) == ROOST_PP_STRINGIZE(elem)) return ROOST_PP_SEQ_ELEM(1,data)::elem ;
+#define EMIT_STRING_TO_ENUM(r, data, elem) if (BOOST_PP_SEQ_ELEM(0,data) == BOOST_PP_STRINGIZE(elem)) return BOOST_PP_SEQ_ELEM(1,data)::elem ;
 
 #define EMIT_STRING_TO_ENUM_CLASS(name) \
 template <> \
 inline name string_to_enum(std::string_view s) { \
-	ROOST_PP_LIST_FOR_EACH( EMIT_STRING_TO_ENUM , (s)(name) , ROOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)) )  \
+	BOOST_PP_LIST_FOR_EACH( EMIT_STRING_TO_ENUM , (s)(name) , BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)) )  \
 	throw std::runtime_error(fmt::format("did not find {}", s)); \
 } \
 template <> \
--- cpp_rutils/require.h
+++ cpp_rutils/require.h
@@ -2,18 +2,18 @@
 
 #include <fmt/format.h>
 #include "name.h"
-#include <roost/preprocessor/variadic/size.hpp>
-#include <roost/preprocessor/if.hpp>
-#include <roost/preprocessor/dec.hpp>
-#include <roost/preprocessor/stringize.hpp>
-#include <roost/preprocessor/variadic/to_tuple.hpp>
-#include <roost/preprocessor/tuple/enum.hpp>
-#include <roost/preprocessor/facilities/is_empty.hpp>
+#include <boost/preprocessor/variadic/size.hpp>
+#include <boost/preprocessor/if.hpp>
+#include <boost/preprocessor/dec.hpp>
+#include <boost/preprocessor/stringize.hpp>
+#include <boost/preprocessor/variadic/to_tuple.hpp>
+#include <boost/preprocessor/tuple/enum.hpp>
+#include <boost/preprocessor/facilities/is_empty.hpp>
 
 #define MREQUIRE2(success,VA_ARGS1,VA_ARGS2) \
 	if (!(success)) throw std::runtime_error( \
 		fmt::format("{} , {} , {} , {} , {}" , __func__ , __FILE__ , __LINE__ ,\
-		  fmt::format(ROOST_PP_TUPLE_ENUM(VA_ARGS1)), fmt::format(ROOST_PP_TUPLE_ENUM(VA_ARGS2))))
+		  fmt::format(BOOST_PP_TUPLE_ENUM(VA_ARGS1)), fmt::format(BOOST_PP_TUPLE_ENUM(VA_ARGS2))))
 
 #define MREQUIRE1(success,...) \
 	if (!(success)) throw std::runtime_error( \
@@ -24,19 +24,19 @@
 	MREQUIRE1(__VA_ARGS__, # __VA_ARGS__ )
 
 #define MREQUIRE(...) \
-	ROOST_PP_IF(ROOST_PP_DEC(ROOST_PP_VARIADIC_SIZE(__VA_ARGS__)) , \
+	BOOST_PP_IF(BOOST_PP_DEC(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__)) , \
 			MREQUIRE1(__VA_ARGS__) , MREQUIRE0(__VA_ARGS__))
 
 #define _MREQUIRE_OP1(op,a,b,...) \
 	MREQUIRE2( (a) op (b) , \
-		ROOST_PP_VARIADIC_TO_TUPLE( ROOST_PP_STRINGIZE(a) ":({}) not "  ROOST_PP_STRINGIZE(op) " " ROOST_PP_STRINGIZE(b) ":({})" , (a) , (b) ) , \
-		ROOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__) )
+		BOOST_PP_VARIADIC_TO_TUPLE( BOOST_PP_STRINGIZE(a) ":({}) not "  BOOST_PP_STRINGIZE(op) " " BOOST_PP_STRINGIZE(b) ":({})" , (a) , (b) ) , \
+		BOOST_PP_VARIADIC_TO_TUPLE(__VA_ARGS__) )
 
 #define _MREQUIRE_OP0(op,a,b) \
-	MREQUIRE1( (a) op (b) , ROOST_PP_STRINGIZE(a) ":({}) not "  ROOST_PP_STRINGIZE(op) " " ROOST_PP_STRINGIZE(b) ":({})" , (a) , (b) )
+	MREQUIRE1( (a) op (b) , BOOST_PP_STRINGIZE(a) ":({}) not "  BOOST_PP_STRINGIZE(op) " " BOOST_PP_STRINGIZE(b) ":({})" , (a) , (b) )
 
 #define _MREQUIRE_OP(op,a,b,...) \
-	ROOST_PP_IF( ROOST_PP_IS_EMPTY(__VA_ARGS__) , \
+	BOOST_PP_IF( BOOST_PP_IS_EMPTY(__VA_ARGS__) , \
 			_MREQUIRE_OP0(op,a,b) , \
 			_MREQUIRE_OP1(op, a, b, __VA_ARGS__) ) 
 
