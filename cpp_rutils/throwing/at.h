a7a04fa96a00efeccd81afc87893da7f0c7ebb36
Richard Rouge <rouge.richard@free.fr>
2022-06-24 23:30:49 +0200

    fix AT

--- cpp_rutils/throwing/at.h
+++ cpp_rutils/throwing/at.h
@@ -1,26 +1,35 @@
 #pragma once
 #include "../require.h"
 
+// at that throw an error with the key when the key is not find
+
 template <typename Container, typename Key>
 inline
-typename const Container::value_type & at(const Container & c , const Key & k)
+typename const Container::mapped_type& at(const Container & c , const Key & k)
 {
 	auto w = c.find(k);
-	if (w==c.end())
-	{
-		MFAIL("could not find key {} in {}", k , type_name<Container>());
-	}
-	return *w;
+	if (w!=c.end())
+		return w->second;
+	MFAIL("could not find key {} in {}", k , type_name<Container>());
 }
 
 template <typename Container, typename Key>
 inline
-typename Container::value_type& at(Container& c, const Key& k)
+typename Container::mapped_type& at(Container& c, const Key& k)
 {
 	auto w = c.find(k);
-	if (w == c.end())
-	{
-		MFAIL("could not find key {} in {}", k, type_name<Container>());
-	}
-	return *w;
+	if (w != c.end())
+		return w->second;
+	MFAIL("could not find key {} in {}", k, type_name<Container>());
 }
+
+// at that throw an error with the key when the key is not find
+// that macro version gibes the actual call site
+
+#define AT(cc,kk) \
+	[](auto &&c, auto &&k) { \
+		auto w = c.find(k); \
+		if (w != c.end()) \
+			return w->second; \
+		[[no_return]] MFAIL("could not find key {} in {}", k, type_name<decltype(c)>()); } \
+		(cc,kk)
--- tests/at.cpp
+++ tests/at.cpp
@@ -1,16 +1,28 @@
 #include <catch2/catch_test_macros.hpp>
 #include <throwing/at.h>
 #include <map>
+#include <unordered_map>
 
 TEST_CASE("throwing at", "[enum][hide]")
 {
 	std::map<int, double> m;
 	m[1] = 1.;
-		at(m, 1);
-		try {
-			at(m, 2);
+
+	CHECK( ( AT(m, 1) ) == 1. );
+	std::unordered_map<int, double> um;
+	um[1] = 1.;
+	// CHECK(at(um, 1) == 1);
+	try {
+			AT(m, 2);
 		}
 	catch(const std::exception &e)
+	{
+		fmt::print("exception {}\n", e.what());
+	}
+	try {
+		AT(um, 2);
+	}
+	catch (const std::exception& e)
 	{
 		fmt::print("exception {}\n", e.what());
 	}
