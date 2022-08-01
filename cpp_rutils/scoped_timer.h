2312f8a86e40ed574669875a1c9dede20bde460c
richard.rouge <rouge.richard@free.fr>
2022-08-01 16:58:28 +0200

    scoped timer

--- cpp_rutils/scoped_timer.h
+++ cpp_rutils/scoped_timer.h
@@ -6,20 +6,38 @@
 class ScopedTimer
 {
 public:
-    ScopedTimer() : _start_time ( std::chrono::steady_clock::now())
-    {
-    }
+	ScopedTimer() : _start_time(std::chrono::steady_clock::now())
+	{
+	}
 
-    ~ScopedTimer()
-    {
-        auto elapsed_time = std::chrono::steady_clock::now() - _start_time;
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time).count();
-        std::cout << "The elapsed time was " << ms << " ms.\n";
-    }
-
+	~ScopedTimer()
+	{
+		auto elapsed_time = std::chrono::steady_clock::now() - _start_time;
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time).count();
+		std::cout << "The elapsed time was " << ms << " ms.\n";
+	}
 
 private:
-    std::chrono::time_point<std::chrono::steady_clock> _start_time ;
+	std::chrono::time_point<std::chrono::steady_clock> _start_time;
 
 };
+
+struct TimeIt
+{
+	TimeIt() : _sc() {}
+	auto        fun(auto&& t) { return std::forward<decltype(t)>(t); }
+	ScopedTimer _sc;
+};
+
+// macro to time a block of code
+// when using with template, encapsulate in double parentheseses
+// e.g. TIMEIT(( std::vector<int>(100) )) ;
+#define TIMEIT(a) TimeIt().fun(a)
+
+
+#define voidTIMEIT(a) \
+  {                   \
+    ScopedTimer _sc;  \
+    a;                \
+  }
 
--- tests/scoped_timer.cpp
+++ tests/scoped_timer.cpp
@@ -3,11 +3,19 @@
 
 #include <thread>
 
+int sleep_for(int ms)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	return ms;
+}
+
 TEST_CASE("scoped timer", "[enum][hide]")
 {
 	{
 		ScopedTimer sc;
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		voidTIMEIT(std::this_thread::sleep_for(std::chrono::milliseconds(10)));
+		TIMEIT(sleep_for(10));
 	}
 }
 
