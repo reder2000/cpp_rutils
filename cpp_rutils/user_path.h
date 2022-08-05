4644c5246f134acdcbd5a63c579f2d5323394aa9
richard.rouge <rouge.richard@free.fr>
2022-08-05 12:33:29 +0200

    path fmt

--- cpp_rutils/user_path.h
+++ cpp_rutils/user_path.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <fmt/format.h>
 
 inline
 std::filesystem::path get_home_path() {
@@ -12,3 +13,15 @@ std::filesystem::path get_home_path() {
 }
 
 
+template <>
+struct fmt::formatter<std::filesystem::path> : formatter<std::string>
+{
+    // parse is inherited from formatter<string_view>.
+    template <typename FormatContext>
+    auto format(std::filesystem::path c, FormatContext& ctx)
+    {
+        std::stringstream ss;
+        ss << c;
+        return formatter<std::string>::format(ss.str(), ctx);
+    }
+};
--- tests/user_path.cpp
+++ tests/user_path.cpp
@@ -2,11 +2,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-
-#include <iostream>
-
-
 TEST_CASE("user_path", "[mem][hide]") {
 	auto foo = get_home_path();
-	std::cout << foo << std::endl;
+	fmt::print("{}", foo);
 }
