215925dead5c0d07604d415e4fe9bb68dcca2a86
richard.rouge <rouge.richard@free.fr>
2022-08-05 11:44:13 +0200

    fix missing declaration

--- cpp_rutils/user_path.h
+++ cpp_rutils/user_path.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include <filesystem>
+
+inline
+std::filesystem::path get_home_path() {
+#if defined(_WIN32)
+	return std::filesystem::path(std::getenv("USERPROFILE"));
+#else // Linux
+	return std::filesystem::path(std::getenv("HOME"));
+#endif
+}
+
+
--- tests/user_path.cpp
+++ tests/user_path.cpp
@@ -2,18 +2,8 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-#include <filesystem>
 
 #include <iostream>
-
-std::filesystem::path get_home_path() {
-#if defined(_WIN32)
-	return std::filesystem::path(std::getenv("USERPROFILE"));
-#else // Linux
-	return std::filesystem::path(std::getenv("HOME"));
-#endif
-}
-
 
 
 TEST_CASE("user_path", "[mem][hide]") {
