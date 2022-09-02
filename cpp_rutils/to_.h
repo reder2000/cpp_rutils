23b231017fa8a5fd0b5a60b31191654cf9a79c2f
Richard Rouge <rouge.richard@free.fr>
2022-09-02 17:45:32 +0200

    multi parameter _to

--- cpp_rutils/to_.h
+++ cpp_rutils/to_.h
@@ -13,13 +13,18 @@ template <typename _To>
 struct to_
 {
   template <typename _From>
-  static _To _(const _From&);
+  static _To _(_From const &);
+  template <typename _From, typename... _MoreArgs>
+  static _To _(_From const &, _MoreArgs  ...)
+  {
+    static_assert(always_false_v<_From, _MoreArgs...>);
+  }
 };
 
 // default implementation
 template <typename _To>
 template <typename _From>
-inline _To to_<_To>::_(const _From &from)
+_To to_<_To>::_(_From const &from)
 {
   static_assert(std::convertible_to<_From, _To>);
   return static_cast<_To>(from);
