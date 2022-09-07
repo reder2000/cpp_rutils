9c7c6d390d13e13a3b7ab7fbecc4230cdcb97b0a
Richard Rouge <rouge.richard@free.fr>
2022-09-07 12:05:32 +0200

    unordered_map with call back insert

--- cpp_rutils/unordered_map.h
+++ cpp_rutils/unordered_map.h
@@ -6,3 +6,27 @@
 
 template <class _Kty, class _Ty, class _Hasher = std::hash<_Kty>, class _Keyeq = std::equal_to<_Kty>>
 using um = std::unordered_map<_Kty, _Ty, _Hasher, _Keyeq>;
+
+// unordered map with a callback function
+// rather than writing value = map.contains(key) ? map[key] , new_value
+// call value = map.find_or_add(key,callback_fn,args_to_fn...)
+
+template <class _Kty, class _Ty, class _Hasher = std::hash<_Kty>, class _Keyeq = std::equal_to<_Kty>>
+struct um_cp : public std::unordered_map<_Kty, _Ty, _Hasher, _Keyeq>
+{
+	using std::unordered_map<_Kty, _Ty, _Hasher, _Keyeq>::unordered_map;
+
+	template <typename  Fn, typename... Args>
+	_Ty& find_or_add(const _Kty& key, Fn fn, Args ...args_to_fn);
+};
+
+template <class _Kty, class _Ty, class _Hasher, class _Keyeq>
+template <typename Fn, typename ... Args>
+_Ty& um_cp<_Kty, _Ty, _Hasher, _Keyeq>::find_or_add(const _Kty& key, Fn fn, Args... args_to_fn)
+{
+	auto w = this->find(key);
+	if (w != this->end()) 
+		return w->second; 
+	auto r = this->emplace(key, fn(args_to_fn...));
+	return r.first->second;
+}
--- tests/unordered_map.cpp
+++ tests/unordered_map.cpp
@@ -8,3 +8,16 @@ TEST_CASE("unordered_map", "[enum][hide]")
 	map[1] = 2.;
 }
 
+
+TEST_CASE("unordered_map_callback", "[enum][hide]")
+{
+	um_cp<int, double> map;
+	map[1] = 2.;
+	CHECK(map.at(1) == 2);
+	auto fns = [](double d) {return d * d; };
+	CHECK(map.find_or_add(1, fns, 2) == 2.);
+	CHECK(map.find_or_add(2, fns, 2) == 4.);
+	auto fnc = []() {return 3.; };
+	CHECK(map.find_or_add(-3, fnc) == 3.);
+}
+
