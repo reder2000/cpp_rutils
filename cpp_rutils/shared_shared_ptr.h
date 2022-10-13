573534f6a51fe110d9c7be27975dc163b0bc0886
Richard Rouge <rouge.richard@free.fr>
2022-10-13 23:18:05 +0200

    shared shared ptr

---
+++ cpp_rutils/shared_shared_ptr.h
@@ -0,0 +1,78 @@
+#pragma once 
+
+//// shorten std::shared_ptr to sp
+//// shorten std::shared_ptr<const ...> to spc
+//// shorten std::unique_ptr to up
+//// shorten std::unique_ptr<const ...> to upc
+//
+//#include <memory>
+//
+//// Shared Pointer
+//template <class _Ty>
+//using sp = std::shared_ptr<_Ty>;
+//
+//// Shared Pointer to Const
+//template <class _Ty>
+//using spc = std::shared_ptr<const _Ty>;
+//
+//// abbreviation for std::make_shared
+//// problems with aggregates & llvm so this does not work #define m_sp std::make_shared
+//#ifdef __clang__
+//template <typename T, typename... Args>
+//std::shared_ptr<T> m_sp(Args&&... args)
+//{
+//	return std::make_shared<T>(T{ std::forward<Args>(args)... });
+//}
+//#else
+//#define m_sp std::make_shared
+//#endif
+//
+//
+//
+//template <class _Ty>
+//sp<_Ty> new_sp() {
+//	return sp<_Ty>(new _Ty);
+//}
+//
+//template <class _Ty>
+//sp<_Ty> new_sp(_Ty&& t) {
+//	return sp<_Ty>(new _Ty(std::forward<_Ty>(t)));
+//}
+//
+//template <class _Ty>
+//spc<_Ty> new_spc(_Ty&& t) {
+//	return spc<_Ty>(new _Ty(std::forward<_Ty>(t)));
+//}
+//
+//template <class _Ty, class _OTy>
+//sp<_Ty> make_sp_copy(const _OTy& t) {
+//	return sp<_Ty>(new _Ty((_OTy(t))));
+//}
+//
+//template <class _Ty, class _OTy>
+//sp<_Ty> make_sp_move(_OTy&& t) {
+//	return sp<_Ty>(new _Ty(std::move(t)));
+//}
+//
+//// Unique Pointer
+//template <class _Ty>
+//using up = std::unique_ptr<_Ty>;
+//
+//// Unique Pointer to Const
+//template <class _Ty>
+//using upc = std::unique_ptr<const _Ty>;
+//
+//template <class _Ty>
+//upc<_Ty> new_upc(_Ty&& t)
+//{
+//	return upc<_Ty>(new _Ty(std::forward<_Ty>(t)));
+//}
+//
+//template <class _Ty>
+//up<_Ty> new_up(_Ty&& t)
+//{
+//	return up<_Ty>(new _Ty(std::forward<_Ty>(t)));
+//}
+//
+//
+//
---
+++ tests/shared_shared_ptr.cpp
@@ -0,0 +1,40 @@
+#include <catch2/catch_test_macros.hpp>
+#include <shared_shared_ptr.h>
+#include <shared_ptr.h>
+
+// shared shared  Pointer
+template <class _Ty>
+using spsp = std::shared_ptr<std::shared_ptr<_Ty>>;
+
+template <class _Ty>
+using spspc = std::shared_ptr<std::shared_ptr<const _Ty>>;
+
+template <typename T, typename... Args>
+spsp<T> m_spsp(Args&&... args)
+{
+	return std::make_shared< std::shared_ptr<T> >(std::make_shared<T>(T{ std::forward<Args>(args)... }));
+}
+
+TEST_CASE("shared_shared_ptr", "[enum][hide]")
+{
+    auto s1 = m_spsp<int>(1);
+    auto s2 = s1;
+    **s2 = 2;
+    CHECK(**s1 == 2);
+    *s2 = m_sp<int>(3);
+    CHECK(**s1 == 3);
+}
+
+//TEST_CASE("shared_ptr_v", "[enum][hide]")
+//{
+//    using V = std::vector<int>;
+//    V v1{ 1,2 };
+//    std::shared_ptr<V> sp1(new V{ 1,2 });
+//    auto sp2 = sp<V>(new V{ 1,2 });
+//    CHECK(*sp1 == *sp2);
+//    auto sp3 = make_sp_move<V>(v1);
+//    CHECK(*sp1 == *sp3);
+//    CHECK(v1.empty());
+//    auto spc1 = new_spc(1);
+//    auto spc2 = m_sp<V>(V{ 1, 2 , 3 });
+//}
