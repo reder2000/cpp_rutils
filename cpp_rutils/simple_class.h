6b5a2e5f2e9084777b025865d8a42edc5deaddb8
Richard Rouge <rouge.richard@free.fr>
2022-12-13 21:31:28 +0100

    pybind simple class

--- cpp_rutils/simple_class.h
+++ cpp_rutils/simple_class.h
@@ -5,12 +5,58 @@
 
 // name_tuple = ((type_1,name_1),(type_2,name_2),...)
 
-// -> type_i name_i
+// -> type_i _name_i ;
 #define EMIT_SIMPLE_CLASS_DECLARE(r, data, i, elem) \
-	BOOST_PP_TUPLE_ELEM(0,elem) BOOST_PP_TUPLE_ELEM(1,elem);
+	BOOST_PP_TUPLE_ELEM(0,elem) BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem));
 
 
-// name => name_tuple_sl = tuple_sl<"a","b",...>
+// ->  type_i _name_i ; ...
 #define EMIT_SIMPLE_CLASS_DECLARE_ALL(name) \
 		BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_DECLARE,name,BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))  
 
+// type_1 name_1 , ...
+#define EMIT_SIMPLE_CLASS_PYBIND11_DECLARE(r, data, i, elem) \
+  BOOST_PP_COMMA_IF(i) BOOST_PP_TUPLE_ELEM(0, elem) BOOST_PP_TUPLE_ELEM(1, elem)
+
+// name_1, ...
+#define EMIT_SIMPLE_CLASS_PYBIND11_LIST_NAMES(r, data, i, elem) \
+  BOOST_PP_COMMA_IF(i) BOOST_PP_TUPLE_ELEM(1, elem)
+
+// .def_readwrite<>("name_1",&simple_class::name1
+#define EMIT_SIMPLE_CLASS_PYBIND11_PROPERTIES(r, data, i, elem)      \
+  .def_readwrite<>(BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)), \
+                                      &data::BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem)))
+// pybind init + properties
+#define EMIT_SIMPLE_CLASS_PYBIND11(name, m)                                                       \
+  py::class_<name>(                                                                               \
+      m,                                                                                          \
+      BOOST_PP_STRINGIZE(name))                                                                   \
+          .def(py::init(                                                                          \
+              [](BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_DECLARE, name,               \
+                                          BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name))))              \
+              {                                                                                   \
+                return name{BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_LIST_NAMES, name, \
+                                                     BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))};  \
+              })) BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_PROPERTIES, name,           \
+                                           BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))
+
+// .def_readwrite<>("name_1",&namespace::simple_class::name1
+#define EMIT_SIMPLE_CLASS_PYBIND11_NPSPC_PROPERTIES(r, data, i, elem)                     \
+  .def_readwrite<>(BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)),                      \
+                                      &BOOST_PP_TUPLE_ELEM(0, data)::BOOST_PP_TUPLE_ELEM( \
+                                          1, data)::BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem)))
+
+// pybind init + properties
+#define EMIT_SIMPLE_NAMESPACE_CLASS_PYBIND11(nmspc, name, m)                                 \
+  py::class_<nmspc::name>(                                                                   \
+      m, BOOST_PP_STRINGIZE(name))                                                           \
+             .def(py::init(                                                                  \
+                 [](BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_DECLARE, name,       \
+                                             BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name))))      \
+                 {                                                                           \
+                   return nmspc::name{                                                       \
+                       BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_LIST_NAMES, name, \
+                                                BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))};  \
+                 }))                                                                         \
+                 BOOST_PP_LIST_FOR_EACH_I(EMIT_SIMPLE_CLASS_PYBIND11_NPSPC_PROPERTIES,       \
+                                          (nmspc, name), BOOST_PP_TUPLE_TO_LIST(NAME_TUPLE(name)))
