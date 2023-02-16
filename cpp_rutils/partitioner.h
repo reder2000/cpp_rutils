0999d91f1322ffdd1fd3d6f72176ec0ec25c6842
Richard Rouge <rouge.richard@free.fr>
2023-02-16 23:58:46 +0100

    fix merge

--- cpp_rutils/partitioner.h
+++ cpp_rutils/partitioner.h
@@ -5,52 +5,51 @@
 
 struct Partitioner
 {
-	Partitioner(size_t total_size, size_t chunk_size, size_t max_nb_cores = 0);
-	Partitioner(const std::vector<size_t>& sizes);
+  Partitioner(size_t total_size, size_t chunk_size, size_t max_nb_cores = 0);
+  Partitioner(const std::vector<size_t>& sizes);
 
-	size_t size() const {
-		return  _chunks.size();
-	}
+  size_t size() const { return _chunks.size(); }
 
-	struct chunk_type
-	{
-		size_t _start;
-		size_t _end;
-	};
+  struct chunk_type
+  {
+    size_t _start;
+    size_t _end;
+  };
 
-	//size_t _nb_parts;
-	//size_t _part_size;
+  //size_t _nb_parts;
+  //size_t _part_size;
 
-	std::vector<chunk_type> _chunks;
+  std::vector<chunk_type> _chunks;
 };
 
 inline Partitioner::Partitioner(size_t total_size, size_t chunk_size, size_t max_nb_cores)
 {
-	size_t nb_cores = max_nb_cores ? max_nb_cores : std::thread::hardware_concurrency();
-	size_t nb_parts = total_size / chunk_size + (unsigned)((total_size % chunk_size) > 0);
-	if (nb_parts > nb_cores) {
-		chunk_size = total_size / nb_cores;
-		nb_parts = nb_cores;
-	}
-	_chunks.reserve(nb_parts);
-	size_t start = 0, end = 0;
-	for (; end < total_size; )
-	{
-		end = std::min(start + chunk_size, total_size);
-		_chunks.push_back({ start, end });
-		start = end;
-	}
+  size_t nb_cores = max_nb_cores ? max_nb_cores : std::thread::hardware_concurrency();
+  size_t nb_parts = total_size / chunk_size + (unsigned)((total_size % chunk_size) > 0);
+  if (nb_parts > nb_cores)
+  {
+    chunk_size = total_size / nb_cores;
+    nb_parts   = nb_cores;
+  }
+  _chunks.reserve(nb_parts);
+  size_t start = 0, end = 0;
+  for (; end < total_size;)
+  {
+    end = std::min(start + chunk_size, total_size);
+    _chunks.push_back({start, end});
+    start = end;
+  }
 }
 
 inline Partitioner::Partitioner(const std::vector<size_t>& sizes)
 {
-	size_t nb_parts = sizes.size();
-	_chunks.reserve(nb_parts);
-	size_t s = 0;
-	for (size_t i = 0; i < nb_parts; i++)
-	{
-		size_t e = s + sizes[i];
-		_chunks.emplace_back(s, e);
-		s = e;
-	}
+  size_t nb_parts = sizes.size();
+  _chunks.reserve(nb_parts);
+  size_t s = 0;
+  for (size_t i = 0; i < nb_parts; i++)
+  {
+    size_t e = s + sizes[i];
+    _chunks.push_back(Partitioner::chunk_type{s, e});
+    s = e;
+  }
 }
--- cpp_rutils/struct_transpose.h
+++ cpp_rutils/struct_transpose.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <vector>
 #include <boost/preprocessor/seq/for_each.hpp>
@@ -31,88 +31,132 @@
 
  */
 
- //#define mystruct_tuple \
+//#define mystruct_tuple \
  //	((int,i),(double,d))
 
-#define name_tuple(name) \
-	BOOST_PP_CAT(name,_tuple)
+#define name_tuple(name) BOOST_PP_CAT(name, _tuple)
 
-#define name_tuple_sequence(name) \
-	BOOST_PP_TUPLE_TO_SEQ(name_tuple(name))
+#define name_tuple_sequence(name) BOOST_PP_TUPLE_TO_SEQ(name_tuple(name))
 
-#define struct_declare_elem(r,data,elem) \
-	BOOST_PP_TUPLE_ELEM(0,elem)  BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem)) ;
+#define struct_declare_elem(r, data, elem) \
+  BOOST_PP_TUPLE_ELEM(0, elem) BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem));
 
-#define  name_tuple_struct_declare(name) \
-	struct name {  \
-		BOOST_PP_SEQ_FOR_EACH(struct_declare_elem,_, name_tuple_sequence(name)) \
-		}
+#define name_tuple_struct_declare(name)                                      \
+  struct name                                                                \
+  {                                                                          \
+    BOOST_PP_SEQ_FOR_EACH(struct_declare_elem, _, name_tuple_sequence(name)) \
+  }
 
- // name_tuple_struct_declare(mystruct);  -> struct mystruct { int _i ; double _d ; } ;
+// name_tuple_struct_declare(mystruct);  -> struct mystruct { int _i ; double _d ; } ;
 
-#define struct_declare_vector_elem(r,data,elem) \
-	std::vector<BOOST_PP_TUPLE_ELEM(0,elem)>  BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem)) ;
+#define struct_declare_vector_elem(r, data, elem) \
+  std::vector<BOOST_PP_TUPLE_ELEM(0, elem)> BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem));
 
-#define struct_declare_vector_init_elem(r,data,i,elem) \
-	BOOST_PP_COMMA_IF(i) BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem))(n)
+#define struct_declare_vector_init_elem(r, data, i, elem) \
+  BOOST_PP_COMMA_IF(i) BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem))(n)
 
-#define  name_tuple_struct_vector_declare(name) \
-	struct BOOST_PP_CAT(name,_vector) {  \
-		BOOST_PP_SEQ_FOR_EACH(struct_declare_vector_elem,_, name_tuple_sequence(name)) \
-		BOOST_PP_CAT(name,_vector) () = default ;\
-		BOOST_PP_CAT(name,_vector)(size_t n) : BOOST_PP_SEQ_FOR_EACH_I(struct_declare_vector_init_elem,_, name_tuple_sequence(name)) {} \
-		size_t size() const { \
-				return BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, BOOST_PP_SEQ_ELEM(0, name_tuple_sequence(name)))).size(); 		} \
-	}
+#define name_tuple_struct_vector_declare(name)                                                     \
+  struct BOOST_PP_CAT(name, _vector)                                                               \
+  {                                                                                                \
+    BOOST_PP_SEQ_FOR_EACH(struct_declare_vector_elem, _, name_tuple_sequence(name))                \
+    BOOST_PP_CAT(name, _vector)() = default;                                                       \
+    BOOST_PP_CAT(name, _vector)                                                                    \
+    (size_t n)                                                                                     \
+        : BOOST_PP_SEQ_FOR_EACH_I(struct_declare_vector_init_elem, _, name_tuple_sequence(name))   \
+    {                                                                                              \
+    }                                                                                              \
+    size_t size() const                                                                            \
+    {                                                                                              \
+      return BOOST_PP_CAT(_,                                                                       \
+                          BOOST_PP_TUPLE_ELEM(1, BOOST_PP_SEQ_ELEM(0, name_tuple_sequence(name)))) \
+          .size();                                                                                 \
+    }                                                                                              \
+  }
 
 // name_tuple_struct_vector_declare(mystruct); -> struct mystruct { std::vector<int> _i ; std::vector<double> _d ; } ;
 
-#define  name_tuple_struct_vector_merge_elem(r,data,elem) \
-	res.BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem)) = merge( v.BOOST_PP_CAT(_,BOOST_PP_TUPLE_ELEM(1,elem))) ;
+#define name_tuple_struct_vector_merge_elem(r, data, elem) \
+  res.BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem)) =      \
+      merge(v.BOOST_PP_CAT(_, BOOST_PP_TUPLE_ELEM(1, elem)));
 
-#define  name_tuple_struct_vector_merge_declare(name) \
-	name merge_struct(const BOOST_PP_CAT(name,_vector)  & v) \
-		{ name res; \
-			BOOST_PP_SEQ_FOR_EACH(name_tuple_struct_vector_merge_elem, X, name_tuple_sequence(name)) \
-			return res; }
+#define name_tuple_struct_vector_merge_declare(name)                                         \
+  name merge_struct(const BOOST_PP_CAT(name, _vector) & v)                                   \
+  {                                                                                          \
+    name res;                                                                                \
+    BOOST_PP_SEQ_FOR_EACH(name_tuple_struct_vector_merge_elem, X, name_tuple_sequence(name)) \
+    return res;                                                                              \
+  }
 
 
 //name_tuple_struct_declare(mystruct);
 //name_tuple_struct_vector_declare(mystruct);
 
 #define name_tuple_struct_vector_merge_declare_all(name) \
-	name_tuple_struct_declare(name);\
-	name_tuple_struct_vector_declare(name);\
-	name_tuple_struct_vector_merge_declare(name)
+  name_tuple_struct_declare(name);                       \
+  name_tuple_struct_vector_declare(name);                \
+  name_tuple_struct_vector_merge_declare(name)
 
 template <class T>
-std::vector<T> merge(const std::vector < std::vector < T>>& vs)
+T merge2(const T& first, const T& second)
 {
-	size_t total_size = std::accumulate(vs.begin(), vs.end(), (size_t)0, [](size_t i, auto const& t) {return i + t.size(); });
-	std::vector<T> res;
-	res.reserve(total_size);
-	for (auto const& s : vs)
-		for (auto const& v : s)
-			res.push_back(v);
-	return res;
+  static_assert(always_false_v<T>);
 }
 
 template <class T>
-std::unordered_set<T>  merge(const std::vector < std::unordered_set < T>>& vs)
+std::vector<T> merge2(const std::vector<T>& first, const std::vector<T>& second)
 {
-	std::unordered_set<T> res;
-	for (auto const& s : vs)
-		for (auto const& v : s)
-			res.insert(v);
-	return res;
+  std::vector<T> res = first;
+  res.insert(res.end(), second.begin(), second.end());
+  return res;
+}
+
+template <class T>
+std::unordered_set<T> merge2(const std::unordered_set<T>& first,
+                             const std::unordered_set<T>& second)
+{
+  std::unordered_set<T> res = first;
+  for (auto const& s : second)
+    res.insert(s);
+  return res;
+}
+
+template <class T>
+std::vector<T> merge(const std::vector<std::vector<T>>& vs)
+{
+  size_t         total_size = std::accumulate(vs.begin(), vs.end(), (size_t)0,
+                                              [](size_t i, auto const& t) { return i + t.size(); });
+  std::vector<T> res;
+  res.reserve(total_size);
+  for (auto const& s : vs)
+    for (auto const& v : s)
+      res.push_back(v);
+  return res;
+}
+
+template <class T>
+std::unordered_set<T> merge(const std::vector<std::unordered_set<T>>& vs)
+{
+  std::unordered_set<T> res;
+  for (auto const& s : vs)
+    for (auto const& v : s)
+      res.insert(v);
+  return res;
 }
 
 template <class T, class U>
-std::unordered_map<T, U>  merge(const std::vector < std::unordered_map < T, U>>& vs)
+std::unordered_map<T, U> merge(const std::vector<std::unordered_map<T, U>>& vs)
 {
-	std::unordered_map<T, U> res;
-	for (auto const& s : vs)
-		for (auto const& v : s)
-			res[v.first] = v.second;
-	return res;
+  std::unordered_map<T, U> res;
+  for (auto const& s : vs)
+    for (auto const& v : s)
+    {
+      auto p = res.find(v.first);
+      if (p == res.end())
+        res[v.first] = v.second;
+      else
+      {
+        res[v.first] = merge2(p->second, v.second);
+      }
+    }
+  return res;
 }
